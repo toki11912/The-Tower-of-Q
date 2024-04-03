b7a6785fdbdd2b859dd8794ca12bc51216f2a422
toki <kazukitmmr5699@gmail.com>
2024-04-04 08:11:20 +0900

    second commit

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Tower
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'xxx.onrender.com'
   end
 end
--- db/migrate/20240111114728_add_title_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToQuestions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :questions, :try, :string
-  end
-end
